8f1c22f19c6b1581c7bb10e7148252a31440a3c3
Damian Wasiak <damianwasiak49@gmail.com>
2023-12-15 00:07:17 +0100

    display posts

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -6,31 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Authorization\IsGranted;
+use App\Repository\PostRepository;
 
 class PostsController extends AbstractController
 {
     
     #[Route('/posts', name: 'app_posts')]
     #[IsGranted('ROLE_USER')]
-    public function index(): Response
+    public function index(PostRepository $postRepository): Response
     {
-         // Przykładowa struktura postów
-         $posts = [
-            [
-                'title' => 'Pierwszy post',
-                'content' => 'Treść pierwszego posta...',
-                'author' => 'Autor 1',
-                'createdAt' => new \DateTime(),
-            ],
-            [
-                'title' => 'Drugi post',
-                'content' => 'Treść drugiego posta...',
-                'author' => 'Autor 2',
-                'createdAt' => new \DateTime(),
-            ],
-            // Dodaj więcej postów według potrzeb
-        ];
-
+        
+        $posts = $postRepository->findAll();
+          
         return $this->render('posts/index.html.twig', [
             'posts' => $posts,
         ]);
