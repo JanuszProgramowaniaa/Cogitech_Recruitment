e8ecfeec8205275cd0ea6e26f61cc05a91b0d615
Damian Wasiak <damianwasiak49@gmail.com>
2023-12-14 23:59:08 +0100

    import posts

--- src/Command/ImportPostsCommand.php
+++ src/Command/ImportPostsCommand.php
@@ -4,17 +4,18 @@ namespace App\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
-use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Console\Output\OutputInterface;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpClient\HttpClient;
+use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\User;
+use App\Entity\Post;
 
 class ImportPostsCommand extends Command
 {
     protected static $defaultName = 'app:import-posts';
-    protected static $defaultDescription = 'Ta importuje użytkowników oraz posty </info>';
-    protected EntityManagerInterface $entityManager;
+    protected static $defaultDescription = 'Importuje użytkowników i posty';
+
+    private EntityManagerInterface $entityManager;
 
     public function __construct(EntityManagerInterface $entityManager)
     {
@@ -22,46 +23,63 @@ class ImportPostsCommand extends Command
         $this->entityManager = $entityManager;
     }
 
-    protected function configure()
-    {
-        // Dodaj opcję --clear, która pozwoli na wyczyszczenie tabeli przed importem
-        $this->addOption('clear', null, null, 'Wyczyść tabelę przed importem');
-    }
-
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-       
-        $this->clearTable('user');
-        $this->resetAutoIncrement('user');
-        $output->writeln('Tabela została wyczyszczona przed importem. Oraz inkrementacja została wyzerowana');
-      
+        $this->deleteUsersAndPosts();
 
+        $this->resetAutoIncrement('user');
+        $output->writeln('Tabela użytkowników została wyczyszczona przed importem. Oraz inkrementacja została wyzerowana');
+
+        $this->importUsers($output);
+
+        $this->resetAutoIncrement('post');
+        $output->writeln('Tabela postów została wyczyszczona przed importem. Oraz inkrementacja została wyzerowana');
+
+        $this->importPosts($output);
+
+        return Command::SUCCESS;
+    }
+
+    private function importUsers(OutputInterface $output)
+    {
         $httpClient = HttpClient::create();
         $usersData = $httpClient->request('GET', 'https://jsonplaceholder.typicode.com/users')->toArray();
 
-        // Inicjalizuj liczniki
         $importedUsersCount = 0;
-        $skippedUsersCount = 0;
 
         foreach ($usersData as $userData) {
             $email = $userData['name'] . '.' . $userData['username'] . '@import.com';
 
-            $existingUser = $this->entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
-
-            if (!$existingUser) {
-                $user = $this->importUser($userData);
-                $importedUsersCount++;
-                $output->writeln('<fg=green>Zaimportowano użytkownika: ' . $user->getUsername() . '</>');
-            } else {
-                $output->writeln('<fg=red>Użytkownik o e-mailu: ' . $email . ' już istnieje</>');
-                $skippedUsersCount++;
-            }
+            $user = $this->importUser($userData);
+            $importedUsersCount++;
+            $output->writeln('<fg=green>Zaimportowano użytkownika: ' . $user->getUsername() . '</>');
+            
         }
 
         $output->writeln('Liczba zaimportowanych użytkowników: <fg=green>' . $importedUsersCount . '</>');
-        $output->writeln('Liczba pominiętych użytkowników: <fg=red>' . $skippedUsersCount . '</>');
+    }
 
-        return Command::SUCCESS;
+    private function importPosts(OutputInterface $output)
+    {
+        $httpClient = HttpClient::create();
+        $postsData = $httpClient->request('GET', 'https://jsonplaceholder.typicode.com/posts')->toArray();
+
+        $importedPostsCount = 0;
+
+        foreach ($postsData as $postData) {
+            $userId = $postData['userId'];
+            $user = $this->entityManager->getRepository(User::class)->find($userId);
+
+            if ($user) {
+                $post = $this->importPost($postData, $user);
+                $importedPostsCount++;
+                $output->writeln('<fg=green>Zaimportowano post: ' . $post->getTitle() . '</>');
+            } else {
+                $output->writeln('<fg=red>Nie znaleziono użytkownika o ID: ' . $userId . ' dla postu o ID: ' . $postData['id'] . '</>');
+            }
+        }
+
+        $output->writeln('Liczba zaimportowanych postów: <fg=green>' . $importedPostsCount . '</>');
     }
 
     private function importUser(array $userData): User
@@ -79,29 +97,42 @@ class ImportPostsCommand extends Command
         return $user;
     }
 
-  
-    private function clearTable($tableName)
+    private function importPost(array $postData, User $user): Post
+    {
+        $post = new Post();
+        $post->setTitle($postData['title']);
+        $post->setBody($postData['body']);
+        $post->setUser($user);
+
+        $doctrine = $this->entityManager;
+        $doctrine->persist($post);
+        $doctrine->flush();
+
+        return $post;
+    }
+
+    private function deleteUsersAndPosts()
     {
         $connection = $this->entityManager->getConnection();
-        $platform = $connection->getDatabasePlatform();
-        $connection->executeStatement($platform->getTruncateTableSQL($tableName, true));
+        
+        $connection->executeStatement('DELETE FROM post');
+        
+        $connection->executeStatement('DELETE FROM user');
     }
 
     private function resetAutoIncrement($tableName)
     {
         $connection = $this->entityManager->getConnection();
         $platform = $connection->getDatabasePlatform();
-   
+
         $tableName = $connection->quoteIdentifier($tableName);
-    
+
         $primaryKey = $connection->getDatabasePlatform()->getIdentifierQuoteCharacter() . 'id' . $connection->getDatabasePlatform()->getIdentifierQuoteCharacter();
 
         $query = "SELECT MAX($primaryKey) as max_id FROM $tableName";
         $maxId = $connection->fetchAssociative($query)['max_id'];
-    
+
         $query = "ALTER TABLE $tableName AUTO_INCREMENT = " . ($maxId + 1);
         $connection->executeStatement($query);
     }
-
-
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
@@ -35,6 +37,14 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $username = null;
+
+    #[ORM\OneToMany(mappedBy: 'User', targetEntity: Post::class, orphanRemoval: true)]
+    private Collection $posts;
+
+    public function __construct()
+    {
+        $this->posts = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -126,6 +136,36 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setUsername(?string $username): static
     {
         $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Post>
+     */
+    public function getPosts(): Collection
+    {
+        return $this->posts;
+    }
+
+    public function addPost(Post $post): static
+    {
+        if (!$this->posts->contains($post)) {
+            $this->posts->add($post);
+            $post->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removePost(Post $post): static
+    {
+        if ($this->posts->removeElement($post)) {
+            // set the owning side to null (unless already changed)
+            if ($post->getUser() === $this) {
+                $post->setUser(null);
+            }
+        }
 
         return $this;
     }
